e2d5b2e0c9f3a7fd60193e4bd241cb15c22d286d
deepak pathak <pathakdeepak024@gmail.com>
2021-06-19 19:23:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./App.css";
+import React, { Component } from "react";
+
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchFeild: ''
+    };
+
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchFeild: e.target.value }) 
+  }
+
+  render() {
+    const {monsters, searchFeild} = this.state;
+    //const monsters = this.state.monsters;
+    //const searchFeild = this.state.searchFeild;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchFeild.toLowerCase())
+      );
+    return (
+      <div className="App">
+        <SearchBox
+        placeholder= 'search monster'
+        handleChange= {this.handleChange}/>
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
